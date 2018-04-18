7d21de6d485a1ec34bdfc3db6d2b9a37904bda81
Jesus Sabroso Centella <jsabroso@e-fti.com>
2018-04-18 10:06:25 +0200

    Basic login.vue testing

---
+++ test/unit/specs/Login.spec.js
@@ -0,0 +1,35 @@
+import { shallow, createLocalVue } from '@vue/test-utils';
+import Login from '@/components/Login';
+import Router from 'vue-router';
+
+const localVue = createLocalVue();
+localVue.use(Router);
+const router = new Router();
+
+describe('Login.vue', () => {
+  it('should render text', () => {
+    const wrapper = shallow(Login, { localVue, router });
+    expect(wrapper.isVueInstance()).toBeTruthy();
+    expect(wrapper.text()).toContain('Login');
+  });
+
+  it('should have password and user empty',()=>{
+    const wrapper = shallow(Login, {localVue,router});
+    expect(wrapper.vm.user).toBe('');
+    expect(wrapper.vm.passwd).toBe('');
+
+    expect(wrapper.vm._data).toHaveProperty('user');
+    expect(wrapper.vm._data).toHaveProperty('passwd');
+  });
+
+  it('should render user login',()=>{
+    const wrapper = shallow(Login, {localVue,router});
+    const fkUser = 'fakeUser';
+    const fkPwd = 'fakePassword'
+
+    wrapper.setData({user:fkUser});
+    wrapper.setData({passwd:fkPwd});
+    expect(wrapper.vm.user).toContain(fkUser);
+    expect(wrapper.vm.passwd).toContain(fkPwd);
+  })
+});
