3ccae1ca5b75f675b38be9fcc4320a7ca48752c3
Xexuline <jscentella@gmail.com>
2018-04-20 14:46:31 +0200

    testing redirect on principalLogin

--- test/unit/specs/PrincipalLogin.spec.js
+++ test/unit/specs/PrincipalLogin.spec.js
@@ -5,11 +5,26 @@ import Router from 'vue-router';
 const localVue = createLocalVue();
 localVue.use(Router);
 const router = new Router();
-
 describe('PrincipalLogin.vue', () => {
   it('should render', () => {
     const wrapper = shallow(PrincipalLogin, { localVue, router });
     expect(wrapper.isVueInstance()).toBeTruthy();
     expect(wrapper.find('.login h1').text()).toBe('Título de twitter');
+  });
+
+  it('redirects to login when login button clicked', () => {
+    const wrapper = shallow(PrincipalLogin, { localVue, router });
+    const button = wrapper.find('a[href="#/login"]');
+    button.trigger('click');
+
+    expect(wrapper.vm.$route.path).toBe('/login');
+  });
+
+  it('redirects to register when register button clicked', () => {
+    const wrapper = shallow(PrincipalLogin, { localVue, router });
+    const button = wrapper.find('a[href="#/register"]');
+    button.trigger('click');
+
+    expect(wrapper.vm.$route.path).toBe('/register');
   });
 });
